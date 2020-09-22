d42acbe9407673d33eb1340777b727a679f85070
Sadsitha Lokuge <slokuge97@gmail.com>
2020-09-22 13:15:43 +0200

    Added pagination

--- script.js
+++ script.js
@@ -67,6 +67,10 @@ function initialise() {
 
     // Init Swiper
     swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
