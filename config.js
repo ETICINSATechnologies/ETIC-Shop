d9a478c59f43a621332d04714359881edc00a998
Sadsitha Lokuge <slokuge97@gmail.com>
2020-10-10 22:01:19 +0200

    Added final products and api keys

--- config.js
+++ config.js
@@ -1,14 +1,18 @@
-var STRIPE_PK =  "pk_test_O0FCm2559gZbRpWia2bR0yVm00Qc7SPLU0";
+var STRIPE_PK =  "pk_live_6BZ3G9A8KUDEM33XiLlliVQe";
 
 var SITE_URL = "https://shop.etic-insa.com";
 
 var PRODUCTS = [
     {
-        id: "pull", name: "Pull ETIC", price: "20€", stripe_price: "price_1HTxf2EUIshe7qGl6WQ1sSjr", image: "images/pull.png",
+        id: "pull", name: "Pull ETIC", price: "20€", stripe_price: "price_1Hao7qGEPGXSOKuWI9MXk4ZB", image: "images/pull.png",
         description: "Pour ceux qui veulent montrer qu'ils sont membres de la meilleure JE de France et ne pas avoir froid à même temps !"
     },
     {
-        id: "masque", name: "Masque ETIC", price: "5€", stripe_price: "price_1HTxuFEUIshe7qGlTvWEaXJ3", image: "images/masque.png",
+        id: "masque", name: "Masque ETIC", price: "3€", stripe_price: "price_1HaoAyGEPGXSOKuW5j1NsOtz", image: "images/masque.png",
         description: "Pour ceux qui veulent montrer qu'ils sont membres de la meilleure JE de France et ne pas propager le covid à même temps !"
+    },
+    {
+        id: "tshirt", name: "T-Shirt ETIC", price: "5€", stripe_price: "price_1HaoDsGEPGXSOKuWjg1rHmhL",
+        description: "Pour ceux qui veulent montrer qu'ils sont membres de la meilleure JE de France !"
     },
 ]
