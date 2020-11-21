804c4c987565bdbdaa2fa75ee57dfce1142d8116
Rahul Ramsaha <45464682+rahulramsaha@users.noreply.github.com>
2020-11-21 16:32:49 +0100

    Added new articles and archived old in a new variable

--- config.js
+++ config.js
@@ -3,16 +3,33 @@ var STRIPE_PK =  "pk_live_6BZ3G9A8KUDEM33XiLlliVQe";
 var SITE_URL = "https://shop.etic-insa.com";
 
 var PRODUCTS = [
+  
     {
-        id: "masque", name: "Masque ETIC", price: 6, stripe_price: "price_1HbX4PGEPGXSOKuWrC9zVWBZ", image: "images/masque.png",
-        description: "Pour ceux qui veulent montrer qu'ils sont membres de la meilleure JE de France et ne pas propager le covid en même temps !"
+        id: "tshirt_blue", name: "T-Shirt Blue ETIC", price: 6, stripe_price: "price_1HcF6LGEPGXSOKuWOFo3F73s", image: "images/tshirt.jpg",
+        description: "Pour ceux qui veulent montrer qu'ils sont membres de la meilleure JE de France !"
     },
     {
-        id: "tshirt", name: "T-Shirt ETIC", price: 6, stripe_price: "price_1HcF6LGEPGXSOKuWOFo3F73s", image: "images/tshirt.jpg",
+        id: "tshirt_white", name: "T-Shirt Blanc ETIC", price: 7.5, stripe_price: "price_1HpxlfGEPGXSOKuWUAftYYUn", image: "images/tshirt_white.jpg",
+        description: "Pour ceux qui veulent montrer qu'ils sont membres de la meilleure JE de France !"
+    },
+    {
+        id: "tshirt_black", name: "T-Shirt Noir ETIC", price: 6, stripe_price: "price_1HpxnVGEPGXSOKuWWEuYYyHP", image: "images/tshirt_black.jpg",
         description: "Pour ceux qui veulent montrer qu'ils sont membres de la meilleure JE de France !"
     },
     {
         id: "pull", name: "Pull ETIC", price: 24, stripe_price: "price_1HcF8IGEPGXSOKuWBqDDntHt", image: "images/pull4.jpg",
         description: "Pour ceux qui veulent montrer qu'ils sont membres de la meilleure JE de France et ne pas avoir froid en même temps !"
     },
+    {
+        id: "bomber", name: "Bomber ETIC", price: 38, stripe_price: "price_1HpxglGEPGXSOKuWGERHsARE", image: "images/bomber.png",
+        description: "Le Bomber offciel d'ETIC est maintenant là ! Indispensable pour les ETICiens qui veulent être élégants et au chaud"
+    }
+]
+
+var OLD_PRODUCTS = [
+    {
+        id: "masque", name: "Masque ETIC", price: 6, stripe_price: "price_1HbX4PGEPGXSOKuWrC9zVWBZ", image: "images/masque.png",
+        description: "Pour ceux qui veulent montrer qu'ils sont membres de la meilleure JE de France et ne pas propager le covid en même temps !"
+    }
+    
 ]
